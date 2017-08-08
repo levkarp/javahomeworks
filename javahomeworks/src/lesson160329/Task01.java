e9c926cf261d529b9bf73ed95f071d5ab1d6f5c7
Администратор <@192.168.1.6>
2017-08-08 20:22:55 +0200

    task 01 done

---
+++ javahomeworks/src/lesson160329/Task01.java
@@ -0,0 +1,22 @@
+package lesson160329;
+
+public class Task01 {
+public static void main(String[] args) {
+	int a=1;
+	int b=2;
+	int c=3;
+	int d=4;
+	int e=5;
+	int f=6;
+	int g=7;
+	int h=8;
+	int l=9;
+	int m=10;
+	System.out.println(a+b+c+d+e+f+g+h+l+m);
+
+
+	
+	
+}
+	
+}
